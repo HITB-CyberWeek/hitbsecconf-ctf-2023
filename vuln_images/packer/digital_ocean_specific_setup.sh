3efbe300b6c258bb047fa7e3f5e4f1e741c599de
Andrey Gein <andgein@yandex.ru>
2023-07-17 11:35:18 +0500

    Fix options in /etc/ssh/sshd_config.d/* too

--- vuln_images/packer/digital_ocean_specific_setup.sh
+++ vuln_images/packer/digital_ocean_specific_setup.sh
@@ -7,6 +7,7 @@ sed -i '/subnets.append(subnet)/a \            if subnet.get("address", "").star
 
 # Set PasswordAuthentication yes
 sed -i '/^[^#]*PasswordAuthentication[[:space:]]no/c\PasswordAuthentication yes' /etc/ssh/sshd_config
+sed -i '/^[^#]*PasswordAuthentication[[:space:]]no/c\PasswordAuthentication yes' /etc/ssh/sshd_config.d/*
 
 # Prohibit to set PasswordAuthentication no
-sed -i 's/updated = update_ssh_config/\    updated = False/' /usr/lib/python3/dist-packages/cloudinit/config/cc_set_passwords.py
+sed -i 's/updated = update_ssh_config\S+/\    updated = False/' /usr/lib/python3/dist-packages/cloudinit/config/cc_set_passwords.py
