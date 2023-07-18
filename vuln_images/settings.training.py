bfbec94ea60c5990882eafaebfdbd3ab1c6a67bc
Andrey Gein <andgein@yandex.ru>
2023-07-18 17:58:54 +0500

    Add support for 250 teams on the training session

--- vuln_images/settings.training.py
+++ vuln_images/settings.training.py
@@ -13,10 +13,12 @@ CERTIFICATES_FOLDER = env.path("CERTIFICATES_FOLDER", "../../certificates")
 
 teams1_100_ctf_hitb_org = CERTIFICATES_FOLDER / "team1.training.ctf.hitb.org"
 teams101_200_ctf_hitb_org = CERTIFICATES_FOLDER / "team101.training.ctf.hitb.org"
+teams201_300_ctf_hitb_org = CERTIFICATES_FOLDER / "team201.training.ctf.hitb.org"
 PROXY_CERTIFICATES = {
     "wildcard.training.ctf.hitb.org": {
         tuple(range(1, 101)): (teams1_100_ctf_hitb_org / "fullchain1.pem", teams1_100_ctf_hitb_org / "privkey1.pem"),
-        tuple(range(100, 201)): (teams101_200_ctf_hitb_org / "fullchain1.pem", teams101_200_ctf_hitb_org / "privkey1.pem"),
+        tuple(range(101, 201)): (teams101_200_ctf_hitb_org / "fullchain1.pem", teams101_200_ctf_hitb_org / "privkey1.pem"),
+        tuple(range(201, 301)): (teams201_300_ctf_hitb_org / "fullchain1.pem", teams201_300_ctf_hitb_org / "privkey1.pem"),
     },
 }
 PROXY_SSH_KEY = env.path("PROXY_SSH_KEY", "../ctf-cloud/cloud/cloud_master/files/api_srv/do_deploy_key")
@@ -28,7 +30,7 @@ TEAM_NETWORK_MASK = env.int("TEAM_NETWORK_MASK", 24)
 
 DNS_ZONE = env.str("DNS_ZONE", "training.ctf.hitb.org")
 
-TEAMS_COUNT = 200
+TEAMS_COUNT = 250
 PROXY_HOSTS = {
     team_id: f"10.80.{team_id}.2" for team_id in range(1, TEAMS_COUNT + 1)
 }
