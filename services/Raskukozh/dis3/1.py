5a9352aeeb4583cfb64a895106815b93ec6b5173
awengar <awengar@gmail.com>
2023-08-01 19:46:08 +0300

    Added obfuscator start code

---
+++ services/Raskukozh/dis3/1.py
@@ -0,0 +1,27 @@
+def five():
+    return 5
+
+def BBB(a,s,b):
+    print(five())
+    a=10
+    a=a-5
+    a=a*5
+    print(a)
+
+    a = 1
+    c = 3
+    for i in range(10):
+        a+=i
+
+    while b < 100:
+        b+=1
+        a+=b
+    if a>=0 and c==3:
+        b=a
+        return
+    else:
+        b=-a
+    return
+print(b)
+print("a")
+print("b")
