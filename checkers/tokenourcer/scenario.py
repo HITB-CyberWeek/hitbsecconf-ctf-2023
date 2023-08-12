caddbdc52ad693690d00c99b9f224ca9dbb5eba1
Daniil Sharko <integrex@yandex.ru>
2023-08-12 03:04:57 +0300

    Change poc

---
+++ checkers/tokenourcer/scenario.py
@@ -0,0 +1,29 @@
+import api
+import generators
+
+hostname = 'localhost'
+
+user_token_name = generators.gen_token_name()
+token_secret = api.issue_token(hostname, user_token_name)
+print('token_secret:', token_secret)
+resource_id = api.create_resource(hostname, token_secret, 'some content')
+print('resource_id:', resource_id)
+resource_blob = api.get_resource(hostname, token_secret, resource_id)
+print('resource_blob:', resource_blob)
+
+hacker_token_name = 'hacker_' + generators.gen_token_name()
+print(hacker_token_name)
+hacker_token_secret = api.issue_token(hostname, hacker_token_name)
+print('hacker_token_secret:', hacker_token_secret)
+hacker_resource_id = api.create_resource(hostname, hacker_token_secret, 'hacker blob')
+api.grant_access(hostname, hacker_token_secret, user_token_name, hacker_resource_id)
+api.grant_access(hostname, hacker_token_secret, user_token_name, hacker_resource_id)
+
+hacker_resource_blob = api.get_resource(hostname, token_secret, hacker_resource_id)
+print('hacker_resource_blob:', hacker_resource_blob)
+
+api.revoke_access(hostname, hacker_token_secret, user_token_name, hacker_resource_id)
+api.revoke_access(hostname, hacker_token_secret, user_token_name, hacker_resource_id)
+
+resource_blob = api.get_resource(hostname, token_secret, resource_id)
+print('resource_blob:', resource_blob)
