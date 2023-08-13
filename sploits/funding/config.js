8d745ada4da1ee56d4aea3b796c9843adea47f6d
Andrey Gein <andgein@yandex.ru>
2023-08-13 18:03:25 +0500

    [funding] Added exploit

---
+++ sploits/funding/config.js
@@ -0,0 +1,9 @@
+export const ethereum = {
+    node: "http://104.248.94.168:8545",
+    accounts: [
+        {
+            address: "0x583D7efd10b585C2e15e8DdaC1d8F341dc97CF9E",
+            // password: "ZzPi9U9wJ3V0Ik9YBB9x88R7J",
+        },
+    ],
+}
