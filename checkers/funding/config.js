f9c6d8c4ed378ef9e4ef12afcaa8578ea107a523
Andrey Gein <andgein@yandex.ru>
2023-08-13 16:25:53 +0500

    [funding] Checker

---
+++ checkers/funding/config.js
@@ -0,0 +1,15 @@
+export const ethereum = {
+    node: "http://104.248.94.168:8545",
+    accounts: [
+        {
+            address: "0x23B42ADe1d5EC963e4D6dA9022eE648639eE93F1",
+            password: "R9f7bPn3HkAehvpFqkfqkrVzZ",
+        },
+        {
+            address: "0x583D7efd10b585C2e15e8DdaC1d8F341dc97CF9E",
+            password: "ZzPi9U9wJ3V0Ik9YBB9x88R7J",
+        },
+    ],
+}
+
+export const passwordSalt = "HITB_2023_FUNDING_PASSWORD_SALT"
