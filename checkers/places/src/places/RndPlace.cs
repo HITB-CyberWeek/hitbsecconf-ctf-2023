6256d4e0a4085721ec6e202a7b951b1698aa48a9
Dmitriy Titarenko <dscheg@users.noreply.github.com>
2023-08-14 23:26:46 +0500

    [places] checker: need more checks

--- checkers/places/src/places/RndPlace.cs
+++ checkers/places/src/places/RndPlace.cs
@@ -8,6 +8,9 @@ public static class RndPlace
 	public static (double Lat, double Long) Coords()
 		=> (Math.Round(RndUtil.Choice(1.0, -1.0) * RndUtil.GetDouble() * 90.0, 6), Math.Round(RndUtil.Choice(1.0, -1.0) * RndUtil.GetDouble() * 180.0, 6));
 
+	public static string RandomPlaceField()
+		=> RndUtil.Bool() ? RndText.RandomWord(RndUtil.GetInt(5, 16)) : RndText.RandomText(RndUtil.GetInt(20, 64));
+
 	public static Place Place()
 	{
 		var (lat, lon) = Coords();
@@ -15,8 +18,8 @@ public static class RndPlace
 		{
 			Lat = lat,
 			Long = lon,
-			Public = RndUtil.Bool() ? RndText.RandomWord(RndUtil.GetInt(5, 16)) : RndText.RandomText(RndUtil.GetInt(20, 64)),
-			Secret = RndUtil.Bool() ? RndText.RandomWord(RndUtil.GetInt(5, 16)) : RndText.RandomText(RndUtil.GetInt(20, 64)),
+			Public = RandomPlaceField(),
+			Secret = RandomPlaceField()
 		};
 	}
 }
