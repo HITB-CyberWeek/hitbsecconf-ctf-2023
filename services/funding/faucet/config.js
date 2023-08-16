761e8fd14b7f6b3aba045cf2af7fea3a4b32c346
Andrey Gein <andgein@yandex.ru>
2023-08-16 22:50:13 +0500

    [funding] Add faucet

---
+++ services/funding/faucet/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    ethereumNode: "http://eth.ctf.hitb.org:8545",
+    address: "0x23B42ADe1d5EC963e4D6dA9022eE648639eE93F1",
+    privateKey: "3e8dbc7a24cbe0aae570178c2a670da8df48aae8c30b7bb1b6d46861accf6962",
+    limitPerNetworkPerHour: 100 // ether
+}
