0f7990504d60093c1502ec3f74e6c5c5b272553b
Alexander Bersenev <bay@hackerdom.ru>
2023-08-18 16:25:37 +0500

    keystone: remove couple comments

--- services/lockstone/start.sh
+++ services/lockstone/start.sh
@@ -4,8 +4,6 @@ chown lockstone:lockstone -R /home/lockstone/
 
 su -s /bin/bash -c "npm install" lockstone
 
-#su -s /bin/bash -c "npm run migrate" lockstone
-
 su -s /bin/bash -c "npm run build" lockstone
 
 su -s /bin/bash -c "npm run migrate_apply" lockstone 
