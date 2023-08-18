de823f2d6937ca57f72e97e79e7afbb3ef128312
Alexander Bersenev <bay@hackerdom.ru>
2023-08-18 18:22:47 +0500

    lockstone: better integration with proxy

--- services/lockstone/start.sh
+++ services/lockstone/start.sh
@@ -2,9 +2,9 @@
 
 chown lockstone:lockstone -R /home/lockstone/
 
+export NODE_ENV=production
+
 su -s /bin/bash -c "npm install" lockstone
-
 su -s /bin/bash -c "npm run build" lockstone
-
 su -s /bin/bash -c "npm run migrate_apply" lockstone 
 exec su -s /bin/bash -c "npm run start --with-migrations" lockstone
