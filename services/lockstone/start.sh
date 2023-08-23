ff11e84e0d881d45e217f2a1338ebb381969fe91
Alexander Bersenev <bay@hackerdom.ru>
2023-08-23 20:42:46 +0500

    lockstone: npm install on the build stage

--- services/lockstone/start.sh
+++ services/lockstone/start.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-chown lockstone:lockstone -R /home/lockstone/
+chown lockstone:lockstone /home/lockstone/keystone.db
 
 export NODE_ENV=production
 
-su -s /bin/bash -c "npm install" lockstone
 su -s /bin/bash -c "npm run build" lockstone
 su -s /bin/bash -c "npm run migrate_apply" lockstone 
 exec su -s /bin/bash -c "npm run start --with-migrations" lockstone
