a37c3adf8c405b3b2743866afb06e4f4d542fbdc
Daniil Sharko <integrex@yandex.ru>
2023-08-25 11:48:18 +0300

    Remove excessstatic check

--- checkers/tokenourcer/checker.py
+++ checkers/tokenourcer/checker.py
@@ -141,11 +141,11 @@ async def check_service(request: CheckRequest) -> Verdict:
         _check_access()
 
         for ((static_category, static_name), expected_hash) in [
-            (["js", "swagger-ui-bundle.js"], "8e799858424d06ec280707ca0c1a81c07c42a221"),
-            (["js", "swagger-ui-standalone-preset.js"], "e35799828884129a0ae2f6bb570273ce150946a6"),
-            (["js", "swagger-initializer.js"], "23fd2279dffdc39c6463c0a0b64f2c0170398f6a"),
+            # (["js", "swagger-ui-bundle.js"], "8e799858424d06ec280707ca0c1a81c07c42a221"),
+            # (["js", "swagger-ui-standalone-preset.js"], "e35799828884129a0ae2f6bb570273ce150946a6"),
+            # (["js", "swagger-initializer.js"], "23fd2279dffdc39c6463c0a0b64f2c0170398f6a"),
             (["css", "index.css"], "71586906338f69420aa4cf1d3494fee8c533f11a"),
-            (["css", "swagger-ui.css"], "bf63ba977691f9755b330a5f64a20289d6b38c86"),
+            # (["css", "swagger-ui.css"], "bf63ba977691f9755b330a5f64a20289d6b38c86"),
         ]:
             if api.get_static_hash(request.hostname, static_category, static_name) != expected_hash:
                 print(f"Invalid static content")
